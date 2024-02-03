688282b73043712b5143b2fbdafbc4ba130f0b73
Ogola Ambala <66788503+OgolaAmbala@users.noreply.github.com>
2024-02-03 16:57:19 +0000

    Completed rb101 easy 1 questions

---
+++ get_the_middle_character.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+def center_of(string)
+  if (string.length % 2).zero?
+    string[(string.length/2 - 1)] + string[(string.length/2)]
+  else
+    string[(string.length/2)]
+  end
+end
+
+p center_of('Nairobi')
+p center_of('London')
