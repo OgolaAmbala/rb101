cc96df216a8bb6c0d9cb2dc4aea3b1afa51a35f8
Ogola Ambala <66788503+OgolaAmbala@users.noreply.github.com>
2024-02-03 16:14:36 +0000

    Completed rb101 reverse the digits exercise

---
+++ reverse_the_digits.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+def reversed_number(num)
+  puts num.to_s.reverse.to_i
+end
+
+reversed_number(12345)
+reversed_number(12213)
+reversed_number(456)
+reversed_number(12000)
+reversed_number(12003)
+reversed_number(1)
