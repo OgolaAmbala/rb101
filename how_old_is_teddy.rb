398de7de666f315ecbf96e1f67986fba236348ed
Ogola Ambala <66788503+OgolaAmbala@users.noreply.github.com>
2024-02-05 09:27:38 +0000

    Amendments to how old is teddy code

--- how_old_is_teddy.rb
+++ how_old_is_teddy.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 def calculate_age(name = 'Teddy')
-  puts "What is your name?"
-  name = gets.chomp
   age = rand(20..200)
   puts "#{name} is #{age} years old"
 end
 
-calculate_age
+calculate_age('James')
