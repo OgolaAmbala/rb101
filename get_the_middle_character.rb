7c6021dc9ce5e3c376b4ccc3f7c4a8f011f80e9a
Ogola Ambala <66788503+OgolaAmbala@users.noreply.github.com>
2024-02-05 06:15:41 +0000

    Completed rb101 get the middle character exercise

--- get_the_middle_character.rb
+++ get_the_middle_character.rb
@@ -2,9 +2,9 @@
 
 def center_of(string)
   if (string.length % 2).zero?
-    string[(string.length/2 - 1)] + string[(string.length/2)]
+    string[(string.length / 2) - 1] + string[(string.length / 2)]
   else
-    string[(string.length/2)]
+    string[(string.length / 2)]
   end
 end
 
