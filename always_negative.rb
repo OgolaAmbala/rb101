42199d62559df1b06fe44a7c9a0defeb97f96e1d
Ogola Ambala <66788503+OgolaAmbala@users.noreply.github.com>
2024-02-06 10:06:05 +0000

    Updated code

--- always_negative.rb
+++ always_negative.rb
@@ -1,21 +1,5 @@
+# frozen_string_literal: true
 
-
-def always_negative(num)
-  case
-    when num > 0
-      puts num * -1
-    when num < 0
-      puts num * -1
-    else
-    puts 0
-  end
+def negative(number)
+  number.positive? ? -number : number
 end
-
-always_negative(-50)
-
-
-=begin
-  def negative(number)
-  number > 0 ? -number : number
-end
-=end
